df0edfd40c1f18c21cb25d5b9c014126f820b808
aryan <aryandangar7@gmail.com>
2021-10-12 14:29:54 +0530

    work 2

--- oreo.py
+++ oreo.py
@@ -2,6 +2,8 @@ import pyttsx3  # need to install sepratly
 import speech_recognition as sr  # need to install sepratly
 import datetime
 import os
+import cv2  # need to install sepratly #pip install opencv-python
+import random
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -48,7 +50,7 @@ def wish():
 
 if __name__ == '__main__':
     wish()
-    #while True:
+    # while True:
     if 1:
         query = takecommand().lower()
 
@@ -61,7 +63,24 @@ if __name__ == '__main__':
             npath = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
             os.startfile(npath)
 
-        elif "open cmd"or "open command prompt" in query:
-           os.system("start cmd")
+        elif "open command prompt" in query:
+            os.system("start cmd")
 
-        #elif "open camera" in query:
+        elif "open camera" in query:
+            cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow('webcam', img)
+                k = cv2.waitKey(50)
+                if k == 27:
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+
+        elif "play music" in query:
+            music_dir = "E:\\Music"
+            songs = os.listdir(music_dir)
+            # rd = random.choice(songs)
+            for song in songs:
+                if song.endswith('.mp3'):
+                    os.startfile(os.path.join(music_dir, song))
