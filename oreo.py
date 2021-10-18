3f4a60751c953a0aac28544d95623603ec6b6dff
aryan <aryandangar7@gmail.com>
2021-10-18 13:58:55 +0530

    work3

--- oreo.py
+++ oreo.py
@@ -4,6 +4,11 @@ import datetime
 import os
 import cv2  # need to install sepratly #pip install opencv-python
 import random
+from requests import get
+import wikipedia # need to install sepratly
+import webbrowser
+import pywhatkit as kit # need to install sepratly
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -84,3 +89,50 @@ if __name__ == '__main__':
             for song in songs:
                 if song.endswith('.mp3'):
                     os.startfile(os.path.join(music_dir, song))
+
+        #elif "ip address" in query:
+        #    ip = get('https://ipify.org').text
+        #    speak(f"your IP Address is {ip}")
+
+        elif "wikipedia" in query:
+            speak("serching wikipedia")
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query,sentences=2)
+            speak("according to wikipedia")
+            speak(results)
+            print(results)
+
+        elif "open youtube"in query:
+            speak("what should i serch on youtube")
+            cm = takecommand().lower()
+            print(cm)
+            if cm == "nothing":
+                webbrowser.open("www.youtube.com")
+            else:
+                kit.playonyt(f"{cm}")
+
+
+        #Social Media
+        elif "open facebook"in query:
+            webbrowser.open("www.facebook.com")
+
+        #elif "Stack overflow"in query:
+            #webbrowser.open("www.stackoverflow.com")
+
+        elif "open google"in query:
+            speak("sir,what should i serch on google")
+            cm = takecommand().lower()
+
+            webbrowser.open(f"{cm}")
+
+        #Sending whatsapp message
+
+        #elif "send message" in query:
+        #kit.sendwhatmsg("+91#numberhere","this is for testing protocol",2,25#time after 2 minuites)
+
+        elif "play song on youtube" in query:
+            speak("sir,wich song should i play")
+            cm = takecommand().lower()
+            kit.playonyt(f"{cm}")
+        
+
